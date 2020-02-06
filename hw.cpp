d1e79bb3a864bdf25f1ced7e2704c2fc52f66129
vasyaivanov1996 <60733115+vasyaivanov1996@users.noreply.github.com>
2020-02-06 12:12:45 +0300

    Add files via upload

---
+++ hw.cpp
@@ -0,0 +1,6 @@
+echo "# helloworld" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/vasyaivanov1996/helloworld.git
+git push -u origin master
